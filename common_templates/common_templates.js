438e11dec2414248649f5a52b98f2650e0726aa3
Pounze Dev <sudeep.ignition@gmail.com>
2017-11-15 03:25:44 +0530

    Add files via upload

--- common_templates/common_templates.js
+++ common_templates/common_templates.js
@@ -1,6 +1,19 @@
-function main()
-{
+const fs = require('fs');
+const session = require('../cns/Haste.js').session;
 
+function main(req,res,input)
+{
+	var header = fs.readFileSync('./views/commonPages/header.html').toString();
+	var name = session.get('fullName');
+	var	designation = session.get('designation');
+
+	var find = ['[@header]','[@name]','[@designation]'];
+	var replace = [header,name,designation];
+
+	return {
+		find:find,
+		replace:replace
+	};
 }
 
 exports.main = main;
